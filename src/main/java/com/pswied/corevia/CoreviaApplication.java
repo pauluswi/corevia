efdb93e74191480ab385da38ab50ae7c3555e473
pauluswi <pswied@gmail.com>
2026-09-22 09:24:13 +0700

    Bootstrapping the project

---
+++ src/main/java/com/pswied/corevia/CoreviaApplication.java
@@ -0,0 +1,12 @@
+package com.pswied.corevia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreviaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreviaApplication.class, args);
+    }
+}
